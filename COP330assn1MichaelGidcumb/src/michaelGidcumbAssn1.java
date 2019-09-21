9663bc5649271607d06aef0169f0ff9b0a70e2d0
SargeSon <mcgidcumb@gmail.com>
2019-09-21 09:31:00 -0400

    remove unrelated files

--- COP330assn1MichaelGidcumb/src/michaelGidcumbAssn1.java
+++
@@ -1,9 +0,0 @@
-//Author: Michael C Gidcumb
-//COP 3330 Programming Assignment 1
-public class michaelGidcumbAssn1 {
-
-	public static void main(String[] args) {
-System.out.println("Hello World!");
-	}
-
-}
